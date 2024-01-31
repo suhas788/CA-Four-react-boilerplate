37269c5a097b804d74c62822253701c793a7f0f5
suhas788 <141901404+suhas788@users.noreply.github.com>
2024-01-31 14:31:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import "./App.css";
 import questions from "./questions";
 import Result from "./components/Result";
 import QuestionBox from "./components/QuestionBox";
 
 
-
 function App() {
+  let [color,setcolor]=useState(true)
+  let[bgColor,setbgColor]=useState('')
+  let[textColor,settextColor]=useState('')
+
+  
+  function backgroundchange(){
+    setcolor(!color)
+    let white='#FFFFFF'
+    let gray='#747474'
+    let black='#000000'
+    setbgColor(color?gray:white)
+    settextColor(color?white:black)
+
+  }
 
   return (
-    <div>
+    <div style={{background: bgColor }} className="body">
+      <div className="head">
+        <h1 style={{color: textColor}}>Kalvium</h1>
+        <button onClick={backgroundchange} className="modeBtn">{color?'Light':'Dark'}</button>
+      </div>
+      <div className="questionBox">
+        <QuestionBox />
+      </div>
       
     </div>
+
   );
 }
 
